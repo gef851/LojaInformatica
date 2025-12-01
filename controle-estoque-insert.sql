e376e9a4024e3c33043b6ec7a439ee1c04da3ca0
gef851 <gef851@hotmail.com>
2025-12-01 20:09:14 -0300

    Update controle-estoque-insert.sql

--- controle-estoque-insert.sql
+++ controle-estoque-insert.sql
@@ -14,10 +14,10 @@ INSERT INTO Peca (nome, codigo, preco, quantidade, id_cliente, id_ordemservico) 
 ('SSD 240Gb', '1396', 299.90, 01, 0003, 00022),
 ('Mouse', '002189', 15.00, 10, 0009, 000091);
 
-INSERT INTO Estoque (data, quantidade, id_peca, id_fornecedor) VALUES
-('2025-11-01', 20, 1, 1),
-('2025-11-02', 10, 2, 2);
+INSERT INTO Estoque (data, quantidade, preco, id_peca, id_ordemservico) VALUES
+('2025-11-28', 05, 130.00, 00011, 000056),
+('2025-11-14', 03, 29.90, 000102, 0000012);
 
-INSERT INTO SaidaVenda (data, quantidade, id_peca, id_cliente) VALUES
-('2025-11-10', 2, 1, 1),
-('2025-11-11', 1, 2, 2);
+INSERT INTO Saida (data, quantidade, id_peca, id_manutencao) VALUES
+('2025-11-02', 02, 00041, 0000211),
+('2025-11-17', 11, 00092, 0000020);
